08252bf57a498ffd42e7f86bf8ed4817afa7fa35
Het Shah <shahhet.1912@gmail.com>
2026-07-09 23:20:12 +0530

    Started Hashing

---
+++ Hashing/Frequency_Count.java
@@ -0,0 +1,16 @@
+public class Frequency_Count {
+  static void Count_Frequency(int[] arr, int n) {
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == n) {
+        count++;
+      }
+    }
+    System.out.println("Frequency of " + n + " is : " + count);
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 3, 4, 1, 2 };
+    Count_Frequency(arr, 1);
+  }
+}
