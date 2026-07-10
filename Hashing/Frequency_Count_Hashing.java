9733b6941e0ddfb7f82d37204bc818085174e7f1
Het Shah <shahhet.1912@gmail.com>
2026-07-10 16:05:55 +0530

    Updated Hashing Program

--- Hashing/Frequency_Count_Hashing.java
+++ Hashing/Frequency_Count_Hashing.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class Frequency_Count_Hashing {
   public static void main(String[] args) {
-
-    int n = 5;
-    int[] arr = {1, 2, 1, 3, 2};
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter No. of Elements in Array : ");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+      System.out.print("Enter Element : ");
+      arr[i] = sc.nextInt();
+    }
 
     // Precompute
     int[] hash = new int[11];
@@ -14,7 +19,6 @@ public class Frequency_Count_Hashing {
 
 
     System.out.print("Enter No. of Queries : ");
-    Scanner sc = new Scanner(System.in);
     int noOfQueries = sc.nextInt();
 
     for(int i = 0; i < noOfQueries; i++){
@@ -23,5 +27,6 @@ public class Frequency_Count_Hashing {
       // Fetch
       System.out.println("Count of "+q+" in Array is : "+hash[q]); 
     }
+    sc.close();
   }
 }
